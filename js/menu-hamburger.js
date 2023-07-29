4dca260449d53e04f958649528116fb9dff9beb5
roddevwork <rodrigocardona87@gmail.com>
2023-07-28 21:12:41 -0300

    responsive updated

--- js/menu-hamburger.js
+++ js/menu-hamburger.js
@@ -1,23 +1,27 @@
 
 /* ********** Menu ********** */
-((d) => {
-  const $btnMenu = d.querySelector(".menu-btn"),
-    $menu = d.querySelector("#menu");
 
-  $btnMenu.addEventListener("click", (e) => {
-    $btnMenu.firstElementChild.classList.toggle("none");
-    $btnMenu.lastElementChild.classList.toggle("none");
-    $menu.classList.toggle("is-active");
-  });
+const $btnMenu = document.querySelector(".menu-btn")
+export const $menu = document.querySelector("#menu")
+const $fraseInspiradora = document.querySelector("#frase-inspiradora")
 
-  d.addEventListener("click", (e) => {
-    if (!e.target.matches(".menu a")) return false;
+$btnMenu.addEventListener("click", (e) => {
+  $btnMenu.firstElementChild.classList.toggle("none");
+  $btnMenu.lastElementChild.classList.toggle("none");
+  $menu.classList.toggle("is-active");
+  $fraseInspiradora.classList.toggle("hidden")
+});
 
-    $btnMenu.firstElementChild.classList.remove("none");
-    $btnMenu.lastElementChild.classList.add("none");
-    $menu.classList.remove("is-active");
-  });
-})(document);
+
+
+
+
+
+
+
+
+
+
 
 
 
--- js/script.js
+++ js/script.js
@@ -1,39 +1,63 @@
 import "/js/menu-hamburger.js"
+import { $menu } from "/js/menu-hamburger.js"
 
 window.addEventListener("load", function () {
 
     //* Variables
-
-
-    //* Functions
-
-
-    //* Listeners
-
-
-
-
-
-
+    let enlaceAtacama = document.getElementById("enlace-atacama");
+    let subMenu = document.querySelector(".submenu")
+    let planDias4 = document.querySelector("#dias-4")
+    let planDias5 = document.querySelector("#dias-5")
+    let planDias6 = document.querySelector("#dias-6")
     let fraseInspiradora = document.getElementById("frase-inspiradora");
     let recuadros = document.getElementById("recuadros");
 
-    // Evento de clic en el enlace "Atacama"
-    let enlaceAtacama = document.getElementById("enlace-atacama");
-
-    enlaceAtacama.addEventListener("click", function (event) {
-        event.preventDefault();
-        fraseInspiradora.style.display = "none";
-        recuadros.style.display = "flex";
-    });
-
-    // Efecto de los recuadros
-    let recuadrosItems = recuadros.getElementsByClassName("recuadro");
-    for (let i = 0; i < recuadrosItems.length; i++) {
-        let recuadro = recuadrosItems[i];
-        recuadro.style.animationDelay = (i * 1) + "s";
-        recuadro.classList.add("recuadro");
+    //* Functions
+    const enlaceAtacamaFn = () => {
+        if (subMenu.classList.contains("selected")) {
+            subMenu.classList.remove("selected")
+        } else {
+            subMenu.classList.add("selected")
+        }
+        subMenu.classList.toggle("none")
     }
+
+    //* Listeners
+    enlaceAtacama.addEventListener("click", enlaceAtacamaFn)
+
+
+
+    //
+    planDias4.addEventListener("click", () => {
+        console.log("click")
+        recuadros.classList.remove("none")
+        $menu.classList.toggle("is-active");
+        fraseInspiradora.style.display = "none"
+    })
+
+    planDias5.addEventListener("click", () => {
+        console.log("click")
+    })
+
+    planDias6.addEventListener("click", () => {
+        console.log("click")
+    })
+
+
+
+    // enlaceAtacama.addEventListener("click", function (event) {
+    //     // event.preventDefault();
+    //     // fraseInspiradora.style.display = "none";
+    //     // recuadros.style.display = "flex";
+    // });
+
+    // // Efecto de los recuadros
+    // let recuadrosItems = recuadros.getElementsByClassName("recuadro");
+    // for (let i = 0; i < recuadrosItems.length; i++) {
+    //     let recuadro = recuadrosItems[i];
+    //     recuadro.style.animationDelay = (i * 1) + "s";
+    //     recuadro.classList.add("recuadro");
+    // }
 
     // Evento de clic en el logo
     let logo = this.document.getElementById("logo");
